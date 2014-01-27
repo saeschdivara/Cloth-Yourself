275f87369ba570c8ab2e5001477d13133330f81a
Sascha Häusler <saeschdivara@gmail.com>
2014-01-27 21:18:58 +0100

    Start

---
+++ ClothingWebsite.cpp
@@ -0,0 +1,56 @@
+/********************************************************************************
+ ** The MIT License (MIT)
+ **
+ ** Copyright (c) 2013 Sascha Ludwig Häusler
+ **
+ ** Permission is hereby granted, free of charge, to any person obtaining a copy of
+ ** this software and associated documentation files (the "Software"), to deal in
+ ** the Software without restriction, including without limitation the rights to
+ ** use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ ** the Software, and to permit persons to whom the Software is furnished to do so,
+ ** subject to the following conditions:
+ **
+ ** The above copyright notice and this permission notice shall be included in all
+ ** copies or substantial portions of the Software.
+ **
+ ** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ ** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ ** FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ ** COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ ** IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ ** CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *********************************************************************************/
+
+#include "ClothingWebsite.h"
+
+#include "views/IndexView.h"
+
+#include <system/web/AbstractSite_p.h>
+
+#include <lib/cachingloaderdecorator.h>
+#include <lib/engine.h>
+#include <lib/template.h>
+
+#include <QtCore/QDebug>
+
+class ClothingWebsitePrivate : public PublicServerSystem::Web::AbstractSitePrivate
+{
+    public:
+};
+
+ClothingWebsite::ClothingWebsite(QObject *parent) :
+    PublicServerSystem::Web::AbstractSite(new ClothingWebsitePrivate, parent)
+{
+    Q_D(ClothingWebsite);
+
+    Grantlee::FileSystemTemplateLoader::Ptr loader( new Grantlee::FileSystemTemplateLoader() );
+    // This path should point to the source of this code
+    loader->setTemplateDirs( QStringList() << "./templates" );
+
+    Grantlee::CachingLoaderDecorator::Ptr cache( new Grantlee::CachingLoaderDecorator( loader ) );
+    d->engine->addTemplateLoader(cache);
+
+    d->engine->addPluginPath("./plugins");
+
+    addView(QLatin1String("^/"), new IndexView);
+}
---
+++ MyLoves_ClothingApp.pro
@@ -0,0 +1,55 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-01-27T20:43:39
+#
+#-------------------------------------------------
+
+QT       += core network
+
+QT       -= gui
+
+TARGET = MyLoves_ClothingApp
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    ClothingWebsite.cpp \
+    views/IndexView.cpp
+
+HEADERS += \
+    ClothingWebsite.h \
+    views/IndexView.h
+
+OTHER_FILES += \
+    templates/index.html
+
+QMAKE_CXXFLAGS += -std=c++11
+CONFIG += c++11
+
+CONFIG(release, debug|release): {
+    LIBS += -L$$PWD/../Public-Server-System/dist/3rdparty/tufao/release/ -ltufao
+    LIBS += -L$$PWD/../Public-Server-System/dist/3rdparty/arangodb-driver/release/ -larangodb-driver
+    LIBS += -L$$PWD/../Public-Server-System/dist/3rdparty/grantlee/template_library/release/ -ltemplate_library
+    LIBS += -L$$PWD/../Public-Server-System/dist/Public-Server-System/release/ -lPublic-Server-System
+}
+else:CONFIG(debug, debug|release): {
+    LIBS += -L$$PWD/../Public-Server-System/dist/3rdparty/tufao/debug/ -ltufao
+    LIBS += -L$$PWD/../Public-Server-System/dist/3rdparty/arangodb-driver/debug/ -larangodb-driver
+    LIBS += -L$$PWD/../Public-Server-System/dist/3rdparty/grantlee/template_library/debug/ -ltemplate_library
+    LIBS += -L$$PWD/../Public-Server-System/dist/Public-Server-System/debug/ -lPublic-Server-System
+}
+
+INCLUDEPATH += $$PWD/../Public-Server-System/3rdparty/tufao/src
+DEPENDPATH += $$PWD/../Public-Server-System/3rdparty/tufao/src
+
+INCLUDEPATH += $$PWD/../Public-Server-System/3rdparty/arangodb-driver/src
+DEPENDPATH += $$PWD/../Public-Server-System/3rdparty/arangodb-driver/src
+
+INCLUDEPATH += $$PWD/../Public-Server-System/3rdparty/grantlee/templates/
+DEPENDPATH += $$PWD/../Public-Server-System/3rdparty/grantlee/templates/
+
+INCLUDEPATH += $$PWD/../Public-Server-System/src/
+DEPENDPATH += $$PWD/../Public-Server-System/src/
